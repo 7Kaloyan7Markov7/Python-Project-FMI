fa3204fa5629514e2c0337c4446e3ab622c7cfce
Kalata <153134271+KokoBoko04@users.noreply.github.com>
2024-11-14 23:37:32 +0200

    Update journey01.py

--- journey.01/journey01.py
+++ journey.01/journey01.py
@@ -44,8 +44,13 @@ animations['up'] = get_all_frames('up_animation.png', 31, 31, 3)
 animations['left'] = get_all_frames('left_animation.png', 31, 31, 3)
 animations['right'] = get_all_frames('right_animation.png', 31, 31, 3)
 
+sword_animations= {'down':get_all_frames('white.png',48,39, 4),
+                    'up':get_all_frames('white.png',48, 39, 4)}
+sword_animations['right'] = get_all_frames('sword_right.png', 48,39,4)
+sword_animations['left'] = get_all_frames('sword_left1.png', 48,39, 4)
 
-sword_animations = get_all_frames('sword_right.png', 48, 39, 4)
+
+sword_IDLE = get_all_frames('sword_right.png', 48, 39, 4)
 sword_x = 400
 sword_y = 100
 is_picked_up = False
@@ -86,16 +91,17 @@ while run:
     screen.blit(floor, (0,0))
     
     player_hit_box = animations['down'][0].get_rect(topleft = (player_x,player_y))
-    sword_pick_up_box = sword_animations[0].get_rect(width  = 80, height = 80, centerx = sword_x + 120, centery = sword_y + 55)
+    sword_pick_up_box = sword_IDLE[0].get_rect(width  = 80, height = 80, centerx = sword_x + 120, centery = sword_y + 55)
 
-    pygame.draw.rect(screen,white,player_hit_box)
-    pygame.draw.rect(screen,white,sword_pick_up_box)
 
     screen.blit(animations[current_direction][current_frame], (player_x, player_y))
-    screen.blit(sword_animations[0], (sword_x,sword_y))
+
+    if not is_picked_up:
+        screen.blit(sword_IDLE[0], (sword_x,sword_y))
+    else:
+        screen.blit(sword_animations[current_direction][0], (sword_x, sword_y))
 
     if pygame.Rect.colliderect(player_hit_box, sword_pick_up_box): is_picked_up = True
-
 
     if not is_picked_up:
         if not is_hit_limit:
@@ -103,14 +109,11 @@ while run:
         else:
             sword_y -= 0.5
 
-
         if sword_y == 124:
             is_hit_limit = True
         elif sword_y == 100:
             is_hit_limit = False
         
-        current_frame += 1
-        if current_frame == 5: current_frame = 0
     else:
         if current_direction == 'left':
             sword_x = player_x - 87
@@ -118,6 +121,9 @@ while run:
         elif current_direction == 'right':
             sword_x = player_x - 20
             sword_y = player_y
+
+    current_frame += 1
+    if current_frame == 5: current_frame = 0
 
 
     pygame.display.update()        
